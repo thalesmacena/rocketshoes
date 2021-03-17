1752cb91331e5a328907aadb7a96160b3dacdfd0
Thales Macena <thalesmacena@gmail.com>
2021-03-17 18:06:46 -0300

    env variable for api url

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 const withImages = require('next-images');
 
 module.exports = withImages({
-  esModule: true
+  esModule: true,
+  env: {
+    API_URL: 'https://rocketshoes-neon.vercel.app/api'
+  }
 });
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://rocketshoes-neon.vercel.app/api'
+  baseURL: process.env.API_URL
 });
