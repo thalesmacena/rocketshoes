e1d556919f5c98611f4ad1527e1e0f9c96bad82e
Thales Macena <thalesmacena@gmail.com>
2021-03-17 13:25:18 -0300

    Main Component

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import { AppProps } from 'next/app';
+import { ToastContainer } from 'react-toastify';
+import Header from '../components/Header/index';
+import { CartProvider } from '../contexts/CartContext';
+import { ThemeProvider } from '../contexts/ThemeContext';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider>
+      <CartProvider>
+        <Header />
+        <Component {...pageProps} />
+        <ToastContainer autoClose={3000} />
+      </CartProvider>
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
