4dc307f30c9b661a06abbb22c3fe208a7e4a6e22
Thales Macena <thalesmacena@gmail.com>
2021-03-19 21:25:23 -0300

    fix on api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL:
     process.env.NODE_ENV === 'production'
-      ? `${process.env.VERCEL_URL}/api`
+      ? `http://${process.env.VERCEL_URL}/api`
       : 'http://localhost:3000/api'
 });
